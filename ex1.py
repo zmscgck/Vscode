02efd1cf635d5a8f01b4e7fe095ffb800da8d733
zmscgck <lgfl@sina.com>
2019-10-12 10:47:17 +0800

    从我的Vscode测试提交

---
+++ ex1.py
@@ -0,0 +1,12 @@
+print ("Hello Word!")
+print ("Hello Again")
+print ("I like typing this.")
+print ("This is fun.")
+print ('Yay! Printing.')
+print ("I'd much rather you 'not'.")
+print ('I "said" do not touch this.')
+print(5+2)
+a=2
+b=3
+c=a*b
+print(c)
---
+++ mypolygon.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 17:19:22 2019
+
+@author: LZM
+"""
+import turtle
+bob=turtle.Turtle()
+print(bob)
+for i in range(4):
+    bob.fd(100)
+    bob.lt(90)
+turtle.mainloop()
+
+
